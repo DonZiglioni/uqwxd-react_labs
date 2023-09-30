2c9f626874f86d6bc846d571c4d3c0e2d2035fd3
DonZiglioni <shift2bass@gmail.com>
2023-09-30 17:27:56 -0400

    Another Edit

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,5 +1,7 @@
 import React, {useState,useEffect} from "react";
 
+// OK NOW WE ARE GETTING SOMEWHERE WITH GIT
+
 import "./App.css";
 const App = () => {
   const [todos, setTodos] = useState([]);
